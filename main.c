d2fdd7108b07690073e0b5827f7709289ab7056d
Dr-Gamaliel <gamankoue@gmail.com>
2025-12-06 20:21:48 +0100

    creation du menu
    Là c'est bon pour le menu 
    La suite c'est creer des structures de données et commencer avec les fonctions principales

--- main.c
+++ main.c
@@ -1,5 +1,51 @@
 #include "tp4.h"
 int main()
 {
+    char** LM;
+    int choix;
+    LM=malloc(sizeof *LM *9);
+    initialisation(LM);
+    do
+    {
+        CONSOLE_SCREEN_BUFFER_INFO BUF;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &BUF);
+        choix=menu(LM,BUF.dwCursorPosition);
+        system("cls");
+        switch (choix)
+        {
+        case 1:
+            printf("---vous etes dans le sous menu \"%s\"---\n",LM[choix]);
+            printf("%s\n",LM[8]);
+            break;
+        case 2:
+            printf("---vous etes dans le sous menu \"%s\"---\n",LM[choix]);
+            printf("%s\n",LM[8]);
+            break;
+        case 3:
+            printf("---vous etes dans le sous menu \"%s\"---\n",LM[choix]);
+            printf("%s\n",LM[8]);
+            break;
+        case 4:
+            printf("---vous etes dans le sous menu \"%s\"---\n",LM[choix]);
+            printf("%s\n",LM[8]);
+            break;
+        case 5:
+            printf("---vous etes dans le sous menu \"%s\"---\n",LM[choix]);
+            printf("%s\n",LM[8]);
+            break;
+        case 6:
+            printf("---vous etes dans le sous menu \"%s\"---\n",LM[choix]);
+            printf("%s\n",LM[8]);
+            break;
+        case 7:
+            printf("---vous etes dans le sous menu \"%s\"---\n",LM[choix]);
+            printf("sortie en cours...\n");
+            break;
+        default:
+            printf("choix etrange\n");
+            break;
+        }
+        system("pause");
+    }while(choix!=7);
     return 0;
 }
--- tp4.h
+++ tp4.h
@@ -2,7 +2,17 @@
 #define TP4_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
+#include <wincon.h>
+#define KBUP 328
+#define KBDOWN 336
 
+void color(int t,int f);
+void gotoxy(int x, int y);
+void initialisation(char** LM);
+int menu(char** tab, COORD O);
 
 
 #endif // TP4_H_INCLUDED
